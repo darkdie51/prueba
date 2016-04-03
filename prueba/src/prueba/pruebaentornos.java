abb1cf3a37746af479c3bc30e0ff3d9ba9524ee3
entorno <entorno@10.0.2.15>
2016-04-03 14:21:51 +0200

    hola

---
+++ prueba/src/prueba/pruebaentornos.java
@@ -0,0 +1,10 @@
+package prueba;
+
+public class pruebaentornos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hola David, como estas?"); //Imprimer en la consola.
+	}
+
+}
